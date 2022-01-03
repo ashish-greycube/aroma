93e5f8d91b33bd77d6bcf3805098924308c01f9c
ashish-greycube <mr.ashish.shah@gmail.com>
2022-01-03 15:34:55 +0530

    feat: flow for QO to SO

--- aroma/booking_controller.py
+++ aroma/booking_controller.py
@@ -7,19 +7,43 @@ def create_booking(self,method):
 	if room_item_group:
 		for item in self.items:
 			if item.item_group==room_item_group:
-				doc = frappe.new_doc('Booking Info')
-				doc.room = item.item_code
-				doc.party_date=self.party_date_cf
-				doc.from_time=self.from_time_cf
-				doc.to_time=self.to_time_cf
-				doc.booked_by=self.owner
-				doc.type_of_booking= 'Initial' if self.doctype =='Quotation' else 'Confirm' 
-				doc.booking_expiry_date=self.valid_till if self.doctype =='Quotation' else  self.booking_expiry_date_cf
-				doc.booked_via=self.doctype
-				doc.reference=self.name
-				doc.save(ignore_permissions=True)
-				msg = _('Booking {} is created for {}'.format(get_link_to_form('Booking Info',doc.name),item.item_code))
-				frappe.msgprint(msg, alert=1)			
+				existing_found=False
+				if self.doctype=='Sales Order':
+					existing_booking_name=frappe.db.sql("""select tbi.name from `tabSales Order` so inner join `tabSales Order Item` sot on so.name=sot.parent 
+inner join `tabBooking Info` tbi  on tbi.reference = sot.prevdoc_docname
+where tbi.room = sot.item_code 
+and tbi.party_date = so.party_date_cf 
+and tbi.from_time = so.from_time_cf 
+and tbi.to_time  = so.to_time_cf 
+and tbi.booked_via = 'Quotation'
+and sot.item_code =%s
+and sot.prevdoc_docname =%s """,(item.item_code,item.prevdoc_docname),as_dict=True)
+					if len(existing_booking_name)>0:
+						doc = frappe.get_doc('Booking Info',existing_booking_name[0].name)
+						doc.update({
+							'type_of_booking':'Confirm',
+							'booking_expiry_date':self.delivery_date,
+							'booked_via':self.doctype,
+							'reference':self.name
+						})
+						doc.save(ignore_permissions=True)
+						existing_found=True
+						msg = _('Existing Booking {} is updated for Sales Order {}'.format(get_link_to_form('Booking Info',doc.name),self.name))
+						frappe.msgprint(msg, alert=1)			
+				if existing_found==False:					
+					doc = frappe.new_doc('Booking Info')
+					doc.room = item.item_code
+					doc.party_date=self.party_date_cf
+					doc.from_time=self.from_time_cf
+					doc.to_time=self.to_time_cf
+					doc.booked_by=self.owner
+					doc.type_of_booking= 'Initial' if self.doctype =='Quotation' else 'Confirm' 
+					doc.booking_expiry_date=self.valid_till if self.doctype =='Quotation' else  self.delivery_date
+					doc.booked_via=self.doctype
+					doc.reference=self.name
+					doc.save(ignore_permissions=True)
+					msg = _('Booking {} is created for {}'.format(get_link_to_form('Booking Info',doc.name),item.item_code))
+					frappe.msgprint(msg, alert=1)			
 
 def after_migrations():
 	update_dashboard_link_for_core_doctype(doctype='Quotation',link_doctype='Booking Info',link_fieldname='reference',group='Reference')
