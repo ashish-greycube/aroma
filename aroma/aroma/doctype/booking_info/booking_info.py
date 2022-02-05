cecb99f5cd99dde35c92868b24c686fbc8014b2d
ashish-greycube <mr.ashish.shah@gmail.com>
2022-02-05 20:10:36 +0530

    fix: data between not working for arabic

--- aroma/aroma/doctype/booking_info/booking_info.py
+++ aroma/aroma/doctype/booking_info/booking_info.py
@@ -22,9 +22,10 @@ def get_events(start, end, filters=None):
 	conditions = get_event_conditions("Booking Info", filters)
 
 	data = frappe.db.sql("""
-select name,room,timestamp(party_date,from_time)as 'start',timestamp(party_date,to_time)as 'end',type_of_booking, IF(type_of_booking='Initial',"#348feb","#aeeb34") as "calendar_color" 
+select name,room,timestamp(party_date,from_time) as `start`,timestamp(party_date,to_time) as `end`,type_of_booking, IF(type_of_booking='Initial',"#348feb","#aeeb34") as "calendar_color" 
 from `tabBooking Info`
-where docstatus < 2 and party_date  between %(start)s and %(end)s {conditions}""".format(conditions=conditions),{"start": start, "end": end}, as_dict=True,debug=True, update={"allDay": 0})
-
-
+where docstatus < 2 
+and '0001-01-01 00:00:00' <= %(start)s
+and '2199-12-31 00:00:00' >= %(end)s
+{conditions}""".format(conditions=conditions),{"start": start, "end": end}, as_dict=True, update={"allDay": 0})
 	return data
